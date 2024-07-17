3a7c8a1489572f81489f3253701610e17eb18fff
Pedroh-dev01 <pedrohdev01@gmail.com>
2024-07-17 19:50:26 -0300

    header responsivo

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const iconHamburger = document.querySelector('.icon__hamburger')
-    const navContainer = document.querySelector('.nav__container')
-    const iconClose = document.querySelector('.icon__close')
+    const header = document.querySelector('header');
+    const iconHamburger = document.querySelector('.icon__hamburger');
+    const iconClose = document.querySelector('.icon__close');
+    const navContainer = document.querySelector('.nav__container');
 
-    iconHamburger.addEventListener('click', function() {
-        navContainer.classList.toggle('active')
-        iconClose.classList.add('active')
-        iconHamburger.classList.remove('active')
+    function toggleMenu() {
+        header.classList.toggle('menu-open');
+        navContainer.classList.toggle('active');
+    }
+
+    iconHamburger.addEventListener('click', toggleMenu);
+    iconClose.addEventListener('click', toggleMenu);
+
+    // Fechar o menu se clicar fora dele
+    document.addEventListener('click', function(event) {
+        const isClickInside = header.contains(event.target);
+        if (!isClickInside && header.classList.contains('menu-open')) {
+            toggleMenu();
+        }
     });
 
-    iconClose.addEventListener('click', function(){
-        navContainer.classList.toggle('active')
-        iconClose.classList.remove('active')
-        iconHamburger.classList.add('active')
-    })
+    // Fechar o menu se a janela for redimensionada para um tamanho maior
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 980 && header.classList.contains('menu-open')) {
+            toggleMenu();
+        }
+    });
 });
